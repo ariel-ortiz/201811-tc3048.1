0ebd6ff33201121932c8d90a1cb7e5a821a1d613
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-01-15 22:03:27 -0600

    First example using regular expressions in C#.

---
+++ 1. Lexical Analysis/regex_example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class RegExExample {
+
+    public static void Main() {
+        var regex = new Regex(@"([_A-Za-z]\w*)|(\d+)");
+        var str = "hello 123 hi_234 0001";
+        foreach (Match m in regex.Matches(str)) {
+            if (m.Groups[1].Success) {
+                Console.WriteLine($"Found an identifier: {m.Value}"
+                    + $" ({m.Index}, {m.Length})"); 
+                
+            } else if (m.Groups[2].Success) {
+                Console.WriteLine($"Found an integer literal: {m.Value}"
+                    + $" ({m.Index}, {m.Length})");
+            }
+        }
+    }
+}
