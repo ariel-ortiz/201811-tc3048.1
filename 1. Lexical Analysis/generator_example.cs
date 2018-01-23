c835b21be28bdc18454b942c96ca1a126cb375aa
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-01-22 19:46:47 -0600

    First example using a generator.

---
+++ 1. Lexical Analysis/generator_example.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GeneratorExample {
+
+    // Generator method
+    public static IEnumerable<int> Start() {
+        var c = 1;
+        while (c < 10000) {
+            yield return c;
+            c *= 2;
+        }
+    }
+
+    public static void Main() {
+        var enumerable = Start();
+        var enumerator = enumerable.GetEnumerator();
+        enumerator.MoveNext();
+        Console.WriteLine(enumerator.Current);
+        enumerator.MoveNext();
+        Console.WriteLine(enumerator.Current);
+        enumerator.MoveNext();
+        Console.WriteLine(enumerator.Current);
+    }
+}
