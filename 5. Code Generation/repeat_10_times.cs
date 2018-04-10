2a6ad85ec1e0e0aaea4c673e9e72426812384910
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-04-09 22:04:38 -0500

    First examples using CIL assembly.

---
+++ 5. Code Generation/repeat_10_times.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Repeat10Times {
+    public static void Main() {
+        for (int i = 0; i < 10; i++) {
+             Console.Write(i);
+             Console.WriteLine(" Hello");
+        }
+    }
+}
+
